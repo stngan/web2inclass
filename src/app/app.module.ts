33f17ea9e3ea4dc06e248a9373ff0b0d4cd72ee4
Tuyet Ngan Ngo Song <ngannst20406c@st.uel.edu.vn>
2023-03-13 18:43:04 +0700

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { Module2JsonComponent } from './module2-json/module2-json.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+=======
+import { FormComponent } from './form/form.component';
+import { DongAbankComponent } from './dong-abank/dong-abank.component';
+
+import { TranslateModule,TranslateLoader  } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient  } from "@angular/common/http";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
+import { FakeProductComponent } from './fake-product/fake-product.component';
+
+import { BitcoinComponent } from './bitcoin/bitcoin.component';
+
+
+
+
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
